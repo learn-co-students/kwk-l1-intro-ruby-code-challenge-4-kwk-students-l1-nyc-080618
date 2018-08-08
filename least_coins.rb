1e946fcaea8eaecffc16e8b63ebf3d3f928d0271
AlinaHaha <2ndalina@gmail.com>
2018-08-08 20:26:15 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,7 @@
-#write out your code here
+def least_coins (189)
+  
+  
+  
+  coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
 
-def least_coins(cents)
-
-#Code your answer here!
-
-end
+end 
