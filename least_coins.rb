1c8054c10b8d159c39c0495434cf03c3530d422d
YingqiTao <yingqitao2000@gmail.com>
2018-08-08 20:12:27 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,25 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
+  number_of_coins = []
+  coins_value = [25, 10, 5, 1]
+  least_coins = {}
+  remainder = cents
+  coins_value_index = 0
+  
+  while remainder != 0
+    number_of_coins.push(remainder/coins_value[coins_value_index])
+    remainder = remainder % coins_value[coins_value_index]
+    coins_value_index += 1
+  end
+  
+  least_coins[:quarters] = number_of_coins[0]
+  least_coins[:dimes] = number_of_coins[1]
+  least_coins[:nickels] = number_of_coins[2]
+  least_coins[:pennies] = number_of_coins[3]
+  
+  return least_coins
 
 end
+
+puts least_coins(29)
